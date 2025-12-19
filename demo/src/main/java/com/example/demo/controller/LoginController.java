5974c7a1d745ec0051d92457eae23eb9ae37b3a1
rivermh <kmh000904@gmail.com>
2025-12-19 11:51:10 +0900

    20251219

--- demo/src/main/java/com/example/demo/controller/LoginController.java
+++ demo/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,6 +1,14 @@
 package com.example.demo.controller;
 
-public class LoginController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class LoginController {
+	
+	@GetMapping("/login")
+	public String loginForm() {
+		return "login";
+	}
 }
  
