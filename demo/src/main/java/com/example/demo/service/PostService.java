c0552fc6cf571db3bb79f543b479093bed76f915
rivermh <kmh000904@gmail.com>
2026-01-07 11:49:34 +0900

    add PostService

--- demo/src/main/java/com/example/demo/service/PostService.java
+++ demo/src/main/java/com/example/demo/service/PostService.java
@@ -2,6 +2,10 @@ package com.example.demo.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -100,4 +104,9 @@ public class PostService {
 		postRepository.delete(post);
 	}
 	
+	//페이징 처리 
+	public Page<Post> getPostPage(int page){
+		Pageable pageable = PageRequest.of(page, 10, Sort.by("id").descending());
+		return postRepository.findAll(pageable);
+	}
 }
